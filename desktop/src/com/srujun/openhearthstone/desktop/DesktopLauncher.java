cf202a06d19351928e6dbbeac0ba76ef45a90431
Srujun Gupta <theununtrium@gmail.com>
2014-05-24 18:47:40 -0500

    Initial libGDX setup.

---
+++ desktop/src/com/srujun/openhearthstone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.srujun.openhearthstone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.srujun.openhearthstone.OHGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OHGame(), config);
+	}
+}
