20a2131e6db3d4293cef81af17a6c4c0cfc3b266
Srujun Gupta <theununtrium@gmail.com>
2014-08-08 15:31:08 +0530

    MINOR CHANGE: renamed arg to args in DesktopLauncher. Meh.

--- desktop/src/com/srujun/openhearthstone/desktop/DesktopLauncher.java
+++ desktop/src/com/srujun/openhearthstone/desktop/DesktopLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.srujun.openhearthstone.OHGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main (String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "OpenHearthstone";
         config.height = 960;
